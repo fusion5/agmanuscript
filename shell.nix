270147c809cc6a1de4706d821768bb88f56de794
Dragos Ionita <dragos.0x55@gmail.com>
2025-01-16 19:30:30 +0000

    dictionary download in xml format

--- shell.nix
+++ shell.nix
@@ -3,16 +3,26 @@ let
   pkgs = import nixpkgs { config = {}; overlays = []; };
   haskellPackages = pkgs.haskellPackages;
   ancientgreek = haskellPackages.callCabal2nix "ancientgreek" ./. {};
+  dictionaries = pkgs.fetchFromGitHub {
+    owner  = "PerseusDL";
+    repo   = "lexica";
+    rev    = "50f2d2430b2dec2b2a0bfe416740cf52f7ea4879";
+    sha256 = "sha256-7HvM3UzYuJ9Y+QTkj4t7W1MQEUXVXeRwB6D3zG9/HX8=";
+  };
+  dictionary_path = n: "CTS_XML_TEI/perseus/pdllex/grc/lsj/grc.lsj.perseus-eng${n}.xml";
 in
   haskellPackages.shellFor {
     packages = p: [
       ancientgreek
     ];
-    withHoogle = false;
+    withHoogle  = false;
+    dict_path   = dictionaries;
+    dict_path_1 = "${dictionaries}/${dictionary_path "1"}";
     buildInputs = with haskellPackages; [
       haskell-language-server
       ghcid
       cabal-install
       hpack
+      # pkgs.readmdict
     ];
   }
