5f88cf45b47e82aec8529ac73784499b41c5781b
Dragos Ionita <dragos.0x55@gmail.com>
2025-01-18 15:41:33 +0000

    Added cli parser and xml library imports

--- shell.nix
+++ shell.nix
@@ -9,7 +9,20 @@ let
     rev    = "50f2d2430b2dec2b2a0bfe416740cf52f7ea4879";
     sha256 = "sha256-7HvM3UzYuJ9Y+QTkj4t7W1MQEUXVXeRwB6D3zG9/HX8=";
   };
-  dictionary_path = n: "CTS_XML_TEI/perseus/pdllex/grc/lsj/grc.lsj.perseus-eng${n}.xml";
+  dictionaryPath = n: "CTS_XML_TEI/perseus/pdllex/grc/lsj/grc.lsj.perseus-eng${n}.xml";
+  # Entry paths:
+  # Word:
+  # xml sel --no-doc-namespace -t -c \
+  #   "/TEI.2/text/body/div0/entryFree[@type=\"main\"]/orth" <file_path>
+  # <entryFree id="n18947" key="a)wti/zomai" type="main" opt="n" TEIform="entryFree">
+  #   <orth extent="suff" lang="greek" opt="n" TEIform="orth">a)wt-i/zomai
+  #   </orth>,
+  #   <orth extent="suff" lang="greek" opt="n" TEIform="orth">a)wt-i/zomai
+  #   </orth>,
+  #   <sense> ... </sense>
+  #   <sense> ... </sense>
+  # </entryFree>
+
 in
   haskellPackages.shellFor {
     packages = p: [
@@ -17,12 +30,13 @@ in
     ];
     withHoogle  = false;
     dict_path   = dictionaries;
-    dict_path_1 = "${dictionaries}/${dictionary_path "1"}";
+    dict_path_1 = "${dictionaries}/${dictionaryPath "1"}";
     buildInputs = with haskellPackages; [
       haskell-language-server
       ghcid
       cabal-install
       hpack
+      pkgs.xmlstarlet
       # pkgs.readmdict
     ];
   }
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,32 @@
 module Main where
 
+import Text.XML.HaXml
+
+import qualified Options.Applicative as Opt
+
+-- type FilePath = String
+
+data CLIArgs = CLIArgs
+  { dictionaryIn :: FilePath
+  } deriving (Show)
+
+cliArgs :: Opt.Parser CLIArgs
+cliArgs
+  = CLIArgs
+    <$> Opt.strOption
+          (   Opt.long "dictionary-in"
+          <>  Opt.metavar "FILE"
+          <>  Opt.help "Input dictionary file in XML format"
+          )
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main
+  = do
+    print "Input file:"
+    print =<< Opt.execParser cliOpts
+    print "XML Parse result:"
+    print $ xmlParse fn body
+  where
+    cliOpts = Opt.info (cliArgs Opt.<**> Opt.helper) Opt.fullDesc
+    fn   = "<TEST>"
+    body =  "<ul><li>1</li><li>2</li></ul>"
