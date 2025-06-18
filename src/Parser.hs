8a813fb81a0266f45ee2febad331c9c3cf5867e7
Dragos I. <dragos.0x55@gmail.com>
2025-06-18 20:58:03 +0200

    Cleanup, don't use operators

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,22 +4,16 @@ import Data.List (stripPrefix)
 
 import qualified Data.Map as Map
 
--- Given a map of terms and symbols, generate all possible parsings of the given string, the
--- map defines a regular language
+type Parser result = String -> [(String, result)]
+
+-- Given a map of terms and symbols, generate all possible parsings of the given string (the
+-- map defines a regular language of a repeating sequence of terms)
 parse :: Map.Map String result -> String -> [[result]]
 parse dict = map snd . just (many $ dictionary dict)
 
 -- Keeps only results that no longer present any remaining input
 just :: Parser symbol -> Parser symbol
 just p = filter ((==) "" . fst) . p
-
-type Parser result = String -> [(String, result)]
-
--- epsilon :: Parser Char ()
--- epsilon xs = [(xs, ())]
-
--- fail :: Parser Char r
--- fail _ = []
 
 succeed :: r -> Parser r
 succeed v xs = [(xs, v)]
@@ -30,24 +24,25 @@ keywordToken prefix newSymbol xs =
     Just rest -> [(rest, newSymbol)]
     Nothing -> []
 
-(<|>) :: Parser a -> Parser a -> Parser a
-(p1 <|> p2) xs = p1 xs ++ p2 xs
+alternative :: Parser a -> Parser a -> Parser a
+alternative p1 p2 xs = p1 xs ++ p2 xs
 
-(<*>) :: (Monoid a) => Parser a -> Parser a -> Parser a
-(p1 <*> p2) xs =
-  [ (xs2, v1 <> v2)
-  | (xs1, v1) <- p1 xs
-  , (xs2, v2) <- p2 xs1
-  ]
-
-many :: (Monoid symbol) => Parser symbol -> Parser symbol
-many p = (p Parser.<*> many p) Parser.<|> succeed mempty
+-- | 0..n occurences of the parser
+many :: Parser symbol -> Parser [symbol]
+many p = (p `parserSequencing` many p) `alternative` succeed []
+ where
+  parserSequencing :: Parser a -> Parser [a] -> Parser [a]
+  parserSequencing p1 p2 xs =
+    [ (xs2, v1 : v2)
+    | (xs1, v1) <- p1 xs
+    , (xs2, v2) <- p2 xs1
+    ]
 
 alts :: [Parser symbol] -> Parser symbol
-alts = foldl (Parser.<|>) mempty
+alts = foldl alternative mempty
 
 dictionaryToAlts :: Map.Map String symbol -> [Parser symbol]
 dictionaryToAlts = map (uncurry keywordToken) . Map.toList
 
-dictionary :: Map.Map String symbol -> Parser [symbol]
-dictionary = alts . dictionaryToAlts . Map.map pure
+dictionary :: Map.Map String symbol -> Parser symbol
+dictionary = alts . dictionaryToAlts
