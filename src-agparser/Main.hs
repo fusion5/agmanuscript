874596f8166731006b26006e5ed5aa8f18844ae5
Dragos I. <dragos.0x55@gmail.com>
2025-09-15 21:01:07 +0200

    Renamed library and first steps in main executable

--- src-agparser/Main.hs
+++ src-agparser/Main.hs
@@ -1,10 +1,32 @@
 module Main (main) where
 
-data CLIArgs = CLIArgs
+import Common
+import Prelude
+
+import qualified Options.Applicative as Opt
+import qualified System.Directory as Dir
+
+newtype CLIArgs = CLIArgs
   { dictionaryFile :: FilePath
   }
+
+cliArgs :: Opt.Parser CLIArgs
+cliArgs =
+  CLIArgs
+    <$> Opt.strOption
+      ( Opt.long "dictionary-file"
+        <> Opt.metavar "FILE"
+        <> Opt.help "Dictionary file in Haskell serialised format")
 
 main :: IO ()
 main =
   do
+    CLIArgs{..} <- Opt.execParser cliOpts
     putStrLn "I'm the Ancient Greek text parser!"
+    exists <- Dir.doesFileExist dictionaryFile
+    unless exists $
+      error
+        [qq|The given dictionary file $dictionaryFile is not accessible|]
+
+ where
+  cliOpts = Opt.info (cliArgs Opt.<**> Opt.helper) Opt.fullDesc
